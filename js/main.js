25a966be7295c5a474091e57b2367887dc6aebd0
Mujakkir85 <mujakkirlucse@gmail.com>
2022-02-24 19:37:24 +0600

    final commit

--- js/main.js
+++ js/main.js
@@ -14,10 +14,14 @@ const loadSearchedFood = () => {
 
 }
 
+//show all meals according to search
+
 const displaySearchFood = (mealsData) => {
     //console.log(mealsData);
     const ShowfoodItems = document.getElementById('show-foods');
-    ShowfoodItems.textContent = '';
+
+    ShowfoodItems.textContent = ''; /// clear previous items
+
     mealsData.forEach(singleMeal => {
         //console.log(singleMeal);
         const div = document.createElement('div');
@@ -41,6 +45,7 @@ const displaySearchFood = (mealsData) => {
 
 }
 
+//get meal details by id
 
 const getFoodItemDetails = (DetailsById) => {
     //console.log(DetailsById);
@@ -49,12 +54,41 @@ const getFoodItemDetails = (DetailsById) => {
 
     fetch(urlById)
         .then(Response => Response.json())
-        .then(dataById => showFoodItemDetails(dataById.meals[0]))
+        .then(dataById => showFoodItemDetails(dataById.meals))
 }
 
+//show meal details by id
 
-const showFoodItemDetails = () => {
-
+const showFoodItemDetails = (singleMealDetails) => {
+    // console.log(singleMealDetails);
     const singleDetails = document.getElementById('single-food-details');
 
+    singleMealDetails.forEach(details => {
+        //console.log(details);
+        singleDetails.textContent = ''; /// clear previous items
+
+        singleDetails.innerHTML = `
+        <img src="${details.strMealThumb}" class="card-img-top " alt="...">
+        <div class="card-body">
+    
+            <h3 class="card-title text-primary">Name: ${details.strMeal}</h3>
+            <h5 class="card-title text-success">Category: ${details.strCategory}</h5>
+            <h5 class="card-title text-success">Origin: ${details.strArea}</h5>
+            <h3 class="text-warning">Ingredient Lists</h3>
+            <ul>    
+                 <li>${details.strIngredient1}</li>
+                 <li>${details.strIngredient2}</li>
+                 <li>${details.strIngredient3}</li>
+                 <li>${details.strIngredient4}</li>
+                 <li>${details.strIngredient5}</li>
+                 <li>${details.strIngredient6}</li>
+            </ul>
+            <p class="card-text">${details.strInstructions.slice(0, 500)}</p>
+    
+        </div>
+        `
+    })
+
 }
+
+
